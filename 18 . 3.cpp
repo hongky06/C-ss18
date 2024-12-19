5bc921a2183a83bd02077a7fb8a332f890cf558a
hongky06 <dohongky2006@gmail.com>
2024-12-19 17:27:47 +0700

    Add files via upload

---
+++ 18 . 3.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main (){
+	struct student {
+		char name[50];
+		int tuoi;
+		char sdt[15];	
+	};
+	struct student so[5];
+
+
+for (int i = 1 ;i < 5;i++){
+	printf("sv so %d ",i);
+	
+	printf("\nten: ");
+	fgets(so[i].name,sizeof(so[i].name),stdin);
+	fflush(stdin);
+	printf("tuoi:");
+	scanf("%d",&so[i].tuoi);
+	fflush(stdin);
+	printf("sdt: ");
+	scanf("%s",&so[i].sdt); 
+	fflush(stdin);
+
+}
+
+
+
+for (int i = 1; i < 5; i++) {
+        printf("Sinh viên %d:\n", i + 1);
+        printf("ten: %s\n", so[i].name);
+        printf("tuoi: %d\n", so[i].tuoi);
+        printf("sdt: %s\n", so[i].sdt);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	return 0;
+}
